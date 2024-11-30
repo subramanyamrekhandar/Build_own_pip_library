2f25c0b6bc769d1f008533744a04b6e912e2ace2
subramanyamrekhandar <subburs987@gmail.com>
2024-11-30 11:59:11 +0530

    basic pip libray code

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='subbutest_library', # package name
+      version='1.0',               # version number
+      author="Subramanya Rekhandar",  # author
+      description="A test library for Basic Calculator ", 
+      packages=['subbutest-library'], # packge directory
+      author_email='aicareerof@gmail.com',
+      zip_safe=False)
---
+++ subbutest-library/__init__.py
@@ -0,0 +1,6 @@
+from .calculatorbasic import add
+from .calculatorbasic import subtract
+from .calculatorbasic import multiply
+from .calculatorbasic import divide
+from .calculatorbasic import greet
+from .calculatorbasic import square
